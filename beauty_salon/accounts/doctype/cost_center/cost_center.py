e8318b065e240813ddfbc6bb3ff766e3cf8c131e
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2023-11-25 02:55:12 +0300

    create app and start work in Stock app
    Create apps:
    Accounts
    Stock
    Setup
    
    and stop in Supplier Link on Item Default modules

---
+++ beauty_salon/accounts/doctype/cost_center/cost_center.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Ahmed-Coding and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class CostCenter(NestedSet):
+	pass
